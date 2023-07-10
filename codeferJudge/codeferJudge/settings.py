a8b9b2c83c9d8150c43f6cf0db4b50f86bb410f6
codfury <shivam04252@gmail.com>
2023-07-10 21:03:11 +0530

    added urls and apps in settings

--- codeferJudge/codeferJudge/settings.py
+++ codeferJudge/codeferJudge/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user.apps.UserConfig',
+    'home.apps.HomeConfig',
+    'problem.apps.ProblemConfig',
 ]
 
 MIDDLEWARE = [
